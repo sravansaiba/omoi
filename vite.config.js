b1d1366152b5ecf15eef7fff981ded2a2ee36002
sravansaiba <sravansaiba@gmail.com>
2025-01-06 16:08:39 +0530

    updating new files

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,4 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-  
-// })
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       external: ['prop-types'], // Externalize prop-types
-//     },
-//   },
-//   optimizeDeps: {
-//     include: ['prop-types'],
-//   },
-  
-// });
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
